d35ae98d265db77126d4ce2bfaed897140b689c2
tamedro <tamedro@Toms-MacBook-Pro.local>
2016-04-02 17:19:59 -0600

    combining behaviours and signals, needs some cleanup

--- Collider.elm
+++ Collider.elm
@@ -9,6 +9,7 @@ import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html exposing (Html, Attribute, text, toElement, div, input)
 import Html.Attributes exposing (..)
+import StartApp.Simple exposing (start)
 
 -- Model
 
@@ -19,9 +20,13 @@ import Html.Attributes exposing (..)
 
 type State = Play | Pause
 
+type Debug = Message | Nothing
+
 type alias Game =
-  { state : State
+  { state : Bool
   , balls : List Ball
+  , debug : String
+  , gun : Gun
   }
 
 type alias Ball =
@@ -32,16 +37,30 @@ type alias Ball =
   , r : Float
   }
 
+type alias Gun =
+  { dir : Int
+  , rotSpeed : Int
+  }
+
+type alias Update =
+  { clickButton : Bool
+  , input : Input
+  }
+
 type alias Input =
   { delta : Time
+  , enter : Bool
+  --, add : Bool
   }
 
 -- defaults
 
 defaultGame : Game
 defaultGame =
-  { state = Pause
+  { state = False
   , balls = []
+  , debug = ""
+  , gun = defaultGun
   }
 
 defaultBall : Ball
@@ -52,51 +71,90 @@ defaultBall =
   , vy = -100
   , r = 15 
   }
-  
+
+defaultGun : Gun
+defaultGun =
+  { dir = 0
+  , rotSpeed = 5
+  }
+
 -- Update
 
-update : Input -> Game -> Game
-update {delta} ({state,balls} as game) =
-    game
+update : Signal.Address Bool ->Update -> Game -> Game
+update address {clickButton, input} ({state,balls,debug,gun} as game) =
+  let
+    newBalls =
+      if clickButton /= state then
+        addBalls balls
+      else 
+        balls
+    
+    newState = clickButton
+    
+    newGun = spinGun gun
+    
+  in 
+    {
+      game |
+        gun = newGun,
+        balls = newBalls,
+        state = newState
+    }
 
 {-
   Adds a  ball to the list of balls
 -}
 addBalls : List Ball -> List Ball
 addBalls balls = defaultBall :: balls
+
+spinGun : Gun -> Gun
+spinGun gun = 
+    { gun | dir = ((gun.dir + gun.rotSpeed) % 360) }
     
 -- View
 
-view : (Int,Int) -> Game -> Element
-view (w,h) game =
+view : Signal.Address Bool -> Bool -> (Int,Int) -> Game -> Element
+view address message (w,h) game =
   flow down [
     (container w h middle <|
       collage gameWidth gameHeight
         [ rect gameWidth gameHeight
           |> filled (rgb 60 60 60)
+        , rect 1 10
+          |> filled red          
+          |> rotate (0 - (degrees (toFloat game.gun.dir)))
         ]
     )
-    , toElement 100 100 (button [buttonStyle] [Html.text "+"])
+    , toElement 100 100 (button [buttonStyle, onClick address (not game.state)] [Html.text (toString (List.length game.balls))])
+    , toElement 100 100 (div [] [Html.text game.debug])
   ]
+
+
+mb : Signal.Mailbox Bool
+mb =
+  Signal.mailbox False
 
 -- Signals
 
 main =
-  Signal.map2 view Window.dimensions gameState
+  Signal.map3 (view mb.address) mb.signal Window.dimensions gameState 
 
 gameState : Signal Game
 gameState =
-  Signal.foldp update defaultGame input
+  Signal.foldp (update mb.address) defaultGame input 
 
 delta =
   Signal.map inSeconds (fps 35)
-  
-input : Signal Input
+
+input : Signal Update
 input =
-  Signal.sampleOn delta <|
-    Signal.map Input
-      delta
-      
+  Signal.map2 Update
+    mb.signal
+    (Signal.sampleOn delta <|
+      Signal.map2 Input
+        delta
+        Keyboard.enter)
+
 -- Styling
 buttonStyle : Attribute
 buttonStyle =
